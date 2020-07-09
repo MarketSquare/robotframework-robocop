3ebba6552b0238be7658ae176b6823f66fb339c1
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2020-07-09 14:43:36 +0200

    add include/exclude rules option

--- robocop/messages/messages.py
+++ robocop/messages/messages.py
@@ -2,7 +2,6 @@ from enum import Enum
 from copy import deepcopy
 
 
-
 class MessageSeverity(Enum):
     INFO = "I"
     WARNING = "W"
--- robocop/run.py
+++ robocop/run.py
@@ -3,6 +3,8 @@ from robot.api import get_model
 import os
 from pathlib import Path
 from robocop import checkers
+from robocop.config import Config
+
 
 SUPPORTED_FORMATS = ('.robot')
 
@@ -10,10 +12,13 @@ SUPPORTED_FORMATS = ('.robot')
 class Robocop:
     def __init__(self):
         self.checkers = []
+        self.config = Config()
+        self.config.parse_opts()
         self.load_checkers()
 
     def run(self):
-        files = sys.argv[1]
+        print(len(self.checkers))
+        files = self.config.paths
         for file in self.get_files(files):
             print(f'Parsing {file}')
             model = get_model(str(file))
@@ -22,30 +27,41 @@ class Robocop:
                 checker.visit(model)
 
     def report(self, msg):
+        if not self.config.is_rule_enabled(msg):
+            return
         print(f"{msg.source}:{msg.line}:{msg.col} [{msg.severity.value}] {msg.msg_id} {msg.desc}")
 
     def load_checkers(self):
         checkers.init(self)
 
     def register_checker(self, checker):
-        self.checkers.append(checker)
+        if self.any_rule_enabled(checker):
+            self.checkers.append(checker)
 
     def get_files(self, files_or_dirs):
         if isinstance(files_or_dirs, list):
             for path in files_or_dirs:
                 yield from self.get_files(path)
-        path = Path(files_or_dirs)
-        if path.is_file() and Robocop.should_parse(path):
-            yield path.absolute()
-        for root, dirs, files in os.walk(files_or_dirs):
-            for name in files:
-                path = Path(root, name)
-                if Robocop.should_parse(path):
-                    yield path.absolute()
+        else:
+            path = Path(files_or_dirs)
+            if path.is_file() and Robocop.should_parse(path):
+                yield path.absolute()
+            for root, dirs, files in os.walk(files_or_dirs):
+                for name in files:
+                    path = Path(root, name)
+                    if Robocop.should_parse(path):
+                        yield path.absolute()
 
     @staticmethod
     def should_parse(file):
         return file.suffix in SUPPORTED_FORMATS
+
+    def any_rule_enabled(self, checker):
+        for msg_name, msg in checker.messages.items():
+            if self.config.is_rule_enabled(msg):
+                return True
+        else:
+            return False
 
 
 def run_robocop():

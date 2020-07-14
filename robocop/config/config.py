7ca42ea225175186c4b6ca710fd01c7fca24d924
Mateusz Nojek <matnojek@gmail.com>
2020-07-14 19:45:22 +0200

    Add parser info and organize help messages

--- robocop/config/config.py
+++ robocop/config/config.py
@@ -2,6 +2,8 @@ import argparse
 import re
 import fnmatch
 
+from robocop.version import __version__
+
 
 class ParseDelimetedArgAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
@@ -36,6 +38,21 @@ class Config:
         self.filetypes = {'.robot', '.resource'}
         self.output = None
 
+    HELP_MSGS = {
+        'help_paths':       'List of paths (files or directories) to be parsed by Robocop',
+        'help_include':     'Run Robocop only with specified rules. You can define rule by its name or id',
+        'help_exclude':     'Ignore specified rules. You can define rule by its name or id',
+        'help_reports':     'Run reports',
+        'help_format':      'Format of output message. '
+                            'You can use placeholders to change the way an issue is reported. '
+                            'Default: {source}:{line}:{col} [{severity}] {msg_id} {desc}',
+        'help_configure':   'Configure checker with parameter value',
+        'help_output':      'Path to output file',
+        'help_filetypes':   'Comma separated list of file extensions to be scanned by Robocop',
+        'help_info':        'Print this help message and exit',
+        'help_version':     'Display Robocop version'
+    }
+
     @staticmethod
     def _translate_pattern(pattern_list):
         return [re.compile(fnmatch.translate(p)) for p in pattern_list if '*' in p]
@@ -45,22 +62,33 @@ class Config:
         self.exclude_patterns = self._translate_pattern(self.exclude)
 
     def parse_opts(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-i', '--include', action=ParseDelimetedArgAction, default=self.include,
-                            help='Run Robocop only with specified rules. You can define rule by its name or id')
-        parser.add_argument('-e', '--exclude', action=ParseDelimetedArgAction, default=self.exclude,
-                            help='Ignore specified rules. You can define rule by its name or id')
-        parser.add_argument('-r', '--reports', action=ParseDelimetedArgAction, default=self.reports,
-                            help='Run reports')
-        parser.add_argument('-f', '--format', type=str, help='Format of output message', default=self.format)
-        parser.add_argument('-c', '--configure', action=ParseCheckerConfig, default=self.configure,
-                            help="Configure checker with parameter value")
-        parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=self.output,
-                            help='Path to output file')
-        parser.add_argument('--filetypes', action=ParseFileTypes, default=self.filetypes,
-                            help='Comma separated list of file extensions to be scanned by Robocop')
-        parser.add_argument('paths', metavar='paths', type=str, nargs='+',
-                            help='List of paths (files and directories) to be scanned by Robocop')
+        parser = argparse.ArgumentParser(prog='robocop',
+                                         description='Static code analysis tool for Robot Framework',
+                                         epilog='For full documentation visit: '
+                                                'https://github.com/bhirsz/robotframework-robocop',
+                                         add_help=False)
+        required = parser.add_argument_group(title='Required parameters')
+        optional = parser.add_argument_group(title='Optional parameters')
+
+        required.add_argument('paths', metavar='paths', type=str, nargs='+', help=self.HELP_MSGS['help_paths'])
+
+        optional.add_argument('-i', '--include', action=ParseDelimetedArgAction, default=self.include,
+                              help=self.HELP_MSGS['help_include'])
+        optional.add_argument('-e', '--exclude', action=ParseDelimetedArgAction, default=self.exclude,
+                              help=self.HELP_MSGS['help_exclude'])
+        optional.add_argument('-r', '--reports', action=ParseDelimetedArgAction, default=self.reports,
+                              help=self.HELP_MSGS['help_reports'])
+        optional.add_argument('-f', '--format', type=str, default=self.format, help=self.HELP_MSGS['help_format'])
+        optional.add_argument('-c', '--configure', action=ParseCheckerConfig, default=self.configure,
+                              help=self.HELP_MSGS['help_configure'])
+        optional.add_argument('-o', '--output', type=argparse.FileType('w'), default=self.output,
+                              help=self.HELP_MSGS['help_output'])
+        optional.add_argument('--filetypes', action=ParseFileTypes, default=self.filetypes,
+                              help=self.HELP_MSGS['help_filetypes'])
+        optional.add_argument('-h', '--help', action='help', help=self.HELP_MSGS['help_info'])
+        optional.add_argument('-v', '--version', action='version', version=__version__,
+                              help=self.HELP_MSGS['help_version'])
+
         args = parser.parse_args()
         self.__dict__.update(**vars(args))
         self.translate_patterns()

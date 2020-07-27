b8a91e0e28910d073a71298e922c69d072891ad0
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2020-07-27 15:41:50 +0200

    pylama issues

--- utest/test_messages.py
+++ utest/test_messages.py
@@ -1,5 +1,5 @@
-import pytest
 import ast
+import pytest
 from robocop.messages import Message, MessageSeverity
 
 
@@ -24,13 +24,13 @@ def valid_msg_with_conf():
     return Message('0101', msg)
 
 
-invalid_msg_missing_severity = (
+INVALID_MSG_MISSING_SEVERITY = (
     "some-message",
     "Some description",
 )
 
 
-invalid_msg_missing_desc_sev = (
+INVALID_MSG_MISSING_DESC_SEV = (
     "some-message"
 )
 
@@ -44,7 +44,7 @@ class TestMessage:
         msg.change_severity(severity)
         return msg.get_fullname()
 
-    @pytest.mark.parametrize('severity, expected_severity', [
+    @pytest.mark.parametrize('severity, exp_sev', [
         ('e', 'E'),
         ('error', 'E'),
         ('i', 'I'),
@@ -56,8 +56,8 @@ class TestMessage:
         ('eRROr', 'E'),
         ('Warning', 'W')
     ])
-    def test_change_message_severity(self, valid_msg, severity, expected_severity):
-        assert TestMessage.change_severity_and_get_fullname(valid_msg, severity) == f"{expected_severity}0101 (some-message)"
+    def test_change_message_severity(self, fixture_valid_msg, severity, exp_sev):
+        assert TestMessage.change_severity_and_get_fullname(fixture_valid_msg, severity) == f"{exp_sev}0101 (some-message)"
 
     @pytest.mark.parametrize('severity', [
         'invalid',
@@ -66,24 +66,24 @@ class TestMessage:
         None,
         dict()
     ])
-    def test_change_message_severity_invalid(self, valid_msg, capsys, severity):
+    def test_change_message_severity_invalid(self, fixture_valid_msg, capsys, severity):
         with pytest.raises(SystemExit):
-            valid_msg.change_severity(severity)
-        out, err = capsys.readouterr()
+            fixture_valid_msg.change_severity(severity)
+        _, err = capsys.readouterr()
         assert str(err) == f"Fatal error: Tried to configure message some-message with invalid severity: {severity}\n"
 
-    def test_get_configurable_existing(self, valid_msg_with_conf, capsys):
-        assert valid_msg_with_conf.get_configurable('param_name') == ('param_name', 'param_priv_name', int)
+    def test_get_configurable_existing(self, fixture_valid_msg_with_conf, capsys):
+        assert fixture_valid_msg_with_conf.get_configurable('param_name') == ('param_name', 'param_priv_name', int)
 
-    def test_get_configurable_empty_configurables(self, valid_msg, capsys):
-        assert valid_msg.get_configurable('param_name') is None
+    def test_get_configurable_empty_configurables(self, fixture_valid_msg, capsys):
+        assert fixture_valid_msg.get_configurable('param_name') is None
 
-    def test_get_configurable_non_existing(self, valid_msg_with_conf, capsys):
-        assert valid_msg_with_conf.get_configurable('invalid_param') is None
+    def test_get_configurable_non_existing(self, fixture_valid_msg_with_conf, capsys):
+        assert fixture_valid_msg_with_conf.get_configurable('invalid_param') is None
 
     @pytest.mark.parametrize('msg', [
-        invalid_msg_missing_severity,
-        invalid_msg_missing_desc_sev,
+        INVALID_MSG_MISSING_SEVERITY,
+        INVALID_MSG_MISSING_DESC_SEV,
         (),
         None,
         1
@@ -91,7 +91,7 @@ class TestMessage:
     def test_parse_invalid_body(self, capsys, msg):
         with pytest.raises(SystemExit):
             Message('0101', msg)
-        out, err = capsys.readouterr()
+        _, err = capsys.readouterr()
         assert str(err) == f"Fatal error: Message '0101' has invalid body:\n{msg}\n"
 
     @pytest.mark.parametrize('configurable', [
@@ -110,7 +110,7 @@ class TestMessage:
         body = msg + tuple(configurable)
         with pytest.raises(SystemExit):
             Message('0101', body)
-        out, err = capsys.readouterr()
+        _, err = capsys.readouterr()
         assert str(err) == f"Fatal error: Message '0101' has invalid configurable:\n{body}\n"
 
     @pytest.mark.parametrize('configurable', [
@@ -132,8 +132,7 @@ class TestMessage:
         ('path/to/file1.robot', None, None, 10, 0),
         ('path/to/file1.robot', 15, None, 15, 0),
         ('path/to/file1.robot', None, 20, 10, 20),
-        ('path/to/file2.robot', 15, 200, 15, 200),
-
+        ('path/to/file2.robot', 15, 200, 15, 200)
     ])
     def test_prepare_message(self, valid_msg, source, lineno, col, lineno_exp, col_exp):
         node = ast.AST()
@@ -150,11 +149,11 @@ class TestMessage:
         (['smth', 1, 10], "Some description %s and %d", "not all arguments converted during string formatting"),
         ([10], "Some description", "not all arguments converted during string formatting"),
     ])
-    def test_prepare_invalid_message_invalid_arg(self, capsys, valid_msg, args, desc, exp_error):
+    def test_prepare_invalid_message_invalid_arg(self, capsys, fixture_valid_msg, args, desc, exp_error):
         node = ast.AST()
         node.lineno = 10
-        valid_msg.desc = desc
+        fixture_valid_msg.desc = desc
         with pytest.raises(SystemExit):
-            valid_msg.prepare_message(*args, source='file1.robot', node=node, lineno=None, col=None)
-        out, err = capsys.readouterr()
+            fixture_valid_msg.prepare_message(*args, source='file1.robot', node=node, lineno=None, col=None)
+        _, err = capsys.readouterr()
         assert str(err) == f"Fatal error: Message '0101' failed to prepare message description with error:{exp_error}\n"

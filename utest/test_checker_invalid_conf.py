696be9671aa676f2a801dec5c38fa2a07fdb4d21
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2020-07-27 15:33:17 +0200

    pylama issues

--- utest/test_checker_invalid_conf.py
+++ utest/test_checker_invalid_conf.py
@@ -2,7 +2,6 @@ import pytest
 from robocop.run import Robocop
 from robocop.checkers import VisitorChecker
 from robocop.messages import MessageSeverity
-from robocop.exceptions import DuplicatedMessageError
 
 
 @pytest.fixture
@@ -59,15 +58,15 @@ class TestCheckerInvalidConf:
     def test_duplicated_message_name_inside_checker(self, capsys, robocop_instance):
         with pytest.raises(SystemExit):
             robocop_instance.register_checker(CheckerDuplicatedMessageName(robocop_instance))
-        out, err = capsys.readouterr()
-        assert str(err) == f"Fatal error: Message name 'some-message' defined in CheckerDuplicatedMessageName " \
-                           f"was already defined in CheckerDuplicatedMessageName\n"
+        _, err = capsys.readouterr()
+        assert str(err) == "Fatal error: Message name 'some-message' defined in CheckerDuplicatedMessageName " \
+                           "was already defined in CheckerDuplicatedMessageName\n"
 
     def test_duplicated_message_name_outside_checker(self, capsys, robocop_instance):
         robocop_instance.register_checker(ValidChecker(robocop_instance))
         with pytest.raises(SystemExit):
             robocop_instance.register_checker(CheckerDuplicatedWithOtherCheckerMessageName(robocop_instance))
-        out, err = capsys.readouterr()
+        _, err = capsys.readouterr()
         assert str(err) == f"Fatal error: Message name 'some-message' defined in " \
                            f"CheckerDuplicatedWithOtherCheckerMessageName was already defined in ValidChecker\n"
 
@@ -75,6 +74,6 @@ class TestCheckerInvalidConf:
         robocop_instance.register_checker(ValidChecker(robocop_instance))
         with pytest.raises(SystemExit):
             robocop_instance.register_checker(CheckerDuplicatedWithOtherCheckerMessageId(robocop_instance))
-        out, err = capsys.readouterr()
+        _, err = capsys.readouterr()
         assert str(err) == f"Fatal error: Message id '0101' defined in " \
                            f"CheckerDuplicatedWithOtherCheckerMessageId was already defined in ValidChecker\n"

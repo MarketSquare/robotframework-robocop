8d1967791afaace1d8678034e908f39ba7a948a1
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2020-09-09 13:17:14 +0200

    move comment rule; change col offset in error rule

--- robocop/checkers/comments.py
+++ robocop/checkers/comments.py
@@ -5,36 +5,8 @@ from robocop.checkers import VisitorChecker
 from robocop.rules import RuleSeverity
 
 
-class CommentBaseChecker(VisitorChecker):
-    """
-    This is base class for comment checkers. Not supposed to be used directly.
-    """
-    rules = {}
-
-    def visit_Comment(self, node):  # noqa
-        self.find_comments(node)
-
-    def visit_TestCase(self, node):  # noqa
-        self.generic_visit(node)
-
-    def visit_Keyword(self, node):  # noqa
-        self.generic_visit(node)
-
-    def visit_KeywordCall(self, node):  # noqa
-        self.find_comments(node)
-        self.generic_visit(node)
-
-    def find_comments(self, node):
-        for token in node.tokens:
-            if token.type == 'COMMENT':
-                self.comment_handler(token)
-
-    def comment_handler(self, comment_token):
-        raise NotImplementedError('CommentExplorer should not be used directly')
-
-
-class CommentChecker(CommentBaseChecker):
-    """ Checker for content of comments. It detects if you have leftover todo or fixme in code. """
+class CommentChecker(VisitorChecker):
+    """ Checker for content of comments. It detects invalid comments or if you have leftover todo or fixme in code. """
     rules = {
         "0701": (
             "todo-in-comment",
@@ -45,10 +17,40 @@ class CommentChecker(CommentBaseChecker):
             "missing-space-after-comment",
             "Missing blank space after comment character",
             RuleSeverity.WARNING
+        ),
+        "0703": (
+            "invalid-comment",
+            "Invalid comment. '#' needs to be first character in the cell. "
+            "For block comments you can use '*** Comments ***' section",
+            RuleSeverity.ERROR
         )
     }
 
-    def comment_handler(self, comment_token):
+    def visit_Comment(self, node):  # noqa
+        self.find_comments(node)
+
+    def visit_TestCase(self, node):  # noqa
+        self.check_invalid_comments(node.name, node)
+        self.generic_visit(node)
+
+    def visit_Keyword(self, node):  # noqa
+        self.check_invalid_comments(node.name, node)
+        self.generic_visit(node)
+
+    def visit_KeywordCall(self, node):  # noqa
+        self.find_comments(node)
+        self.generic_visit(node)
+
+    def find_comments(self, node):
+        for token in node.tokens:
+            if token.type == 'COMMENT':
+                self.check_comment_content(token)
+
+    def check_invalid_comments(self, name, node):
+        if name and name.lstrip().startswith('#'):
+            self.report("invalid-comment", node=node, col=node.col_offset)
+
+    def check_comment_content(self, comment_token):
         if 'todo' in comment_token.value.lower():
             self.report("todo-in-comment", "TODO", lineno=comment_token.lineno, col=comment_token.col_offset)
         if "fixme" in comment_token.value.lower():

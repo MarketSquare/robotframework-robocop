82fdb2c381263ed6b702b77ce946635ffec9945a
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2021-02-27 19:54:03 +0100

    fix tests failing due to missing .git file inside test data

--- tests/utest/test_default_config.py
+++ tests/utest/test_default_config.py
@@ -20,10 +20,9 @@ class TestDefaultConfig:
 
     def test_find_project_root_missing_but_git(self, config):
         src = Path(__file__).parent.parent / 'test_data' / 'default_config_missing' / 'nested' / 'deeper'
-        expected_root = Path(__file__).parent.parent / 'test_data' / 'default_config_missing'
         os.chdir(str(src))
         root = config.find_project_root()
-        assert root == expected_root
+        assert root == Path(__file__).parent.parent.parent
 
     def test_load_config_from_default_file(self, config):
         src = Path(__file__).parent.parent / 'test_data' / 'default_config'

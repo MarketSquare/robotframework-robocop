26cad918c154d428acfacd53a5bae101ff5ac5ab
Mateusz Nojek <matnojek@gmail.com>
2021-03-11 11:43:03 +0100

    Organize setup.py and add more keywords

--- setup.py
+++ setup.py
@@ -20,26 +20,29 @@ Topic :: Software Development :: Quality Assurance
 Topic :: Utilities
 Intended Audience :: Developers
 """.strip().splitlines()
+KEYWORDS = ('robotframework automation testautomation testing linter qa')
+DESCRIPTION = ('Static code analysis tool (linter) for Robot Framework')
 
 setup(
-    name='robotframework-robocop',
-    version=__version__,
-    description='Static code analysis tool (linter) for Robot Framework',
-    long_description=README,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/MarketSquare/robotframework-robocop",
-    author="Bartlomiej Hirsz, Mateusz Nojek",
-    author_email="bartek.hirsz@gmail.com, matnojek@gmail.com",
-    license="Apache License 2.0",
-    platforms="any",
-    classifiers=CLASSIFIERS,
-    keywords='robotframework',
-    packages=['robocop'],
-    include_package_data=True,
-    install_requires=['robotframework>=3.2.2'],
-    extras_requires={
+    name         = 'robotframework-robocop',
+    version      = __version__,
+    description  = DESCRIPTION,
+    long_description = README,
+    long_description_content_type = 'text/x-rst',
+    url          = 'https://github.com/MarketSquare/robotframework-robocop',
+    download_url = 'https://pypi.org/project/robotframework-robocop',
+    author       = 'Bartlomiej Hirsz, Mateusz Nojek',
+    author_email = 'bartek.hirsz@gmail.com, matnojek@gmail.com',
+    license      = 'Apache License 2.0',
+    platforms    = 'any',
+    classifiers  = CLASSIFIERS,
+    keywords     = KEYWORDS,
+    packages     = ['robocop'],
+    include_package_data = True,
+    install_requires = ['robotframework>=3.2.2'],
+    extras_requires = {
         'dev': ['pytest'],
         'doc': ['sphinx', 'sphinx_rtd_theme']
     },
-    entry_points={'console_scripts': ['robocop=robocop:run_robocop']},
+    entry_points = {'console_scripts': ['robocop=robocop:run_robocop']},
 )

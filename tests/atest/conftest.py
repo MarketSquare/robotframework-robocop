ace74bedc4a24138505734802108811d33eb42eb
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2021-04-02 09:32:59 +0200

    add docs to test generator

--- tests/atest/conftest.py
+++ tests/atest/conftest.py
@@ -11,10 +11,20 @@ def robocop_instance():
 
 
 def pytest_generate_tests(metafunc):
+    """ This method generate tests based on template ``test_rule``.
+
+    Rules are autodiscovered. You can add your own tests by adding tuple with test data to ``auto_discovered_rules``
+    list. This tuple should contain:
+    ('name_of_rule_to_be_included', ['list', 'of', 'cli', 'extra', 'args'], 'path/to/src/and/expected inside rules dir')
+
+    For example::
+
+        ('line-too-long', ['--configure', 'line-too-long:line_length:1'], 'lengths/line-too-long')
+
+    """
     if "rule" not in metafunc.fixturenames:
         return
     auto_discovered_rules = [(rule, None, f"{category}/{rule}") for category, rule in get_rules_for_atest()]
-    # auto_discovered_rules.append(('line-too-long', ['--configure', 'line-too-long:line_length:1'], 'line-too-long'))
     metafunc.parametrize('rule, args, test_data', auto_discovered_rules)
 
 

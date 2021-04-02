c85ef6ef2242b4118d5a677298a070615ce3921e
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2021-04-02 15:42:52 +0200

    fix typo
    Co-authored-by: Mateusz Nojek <matnojek@gmail.com>

--- robocop/checkers/misc.py
+++ robocop/checkers/misc.py
@@ -148,7 +148,7 @@ class ConsistentAssignmentSignChecker(VisitorChecker):
     rules = {
         "0909": (
             "inconsistent-assignment",
-            "The assignment sign is not consistent thorough the file. Expected '%s' but got '%s' instead",
+            "The assignment sign is not consistent through the file. Expected '%s' but got '%s' instead",
             RuleSeverity.WARNING,
             ('assignment_sign_type', 'keyword_assignment_sign_type', parse_assignment_sign_type)
         ),

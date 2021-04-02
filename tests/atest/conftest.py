c02a3e7221c5d75f74db7f673136d39804179e26
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2021-04-02 09:44:01 +0200

    remove not used method

--- tests/atest/conftest.py
+++ tests/atest/conftest.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 from robocop.checkers import get_rules_for_atest
-from robocop.run import Robocop, Config
+from robocop.run import Robocop
 
 
 @pytest.fixture
@@ -26,19 +26,3 @@ def pytest_generate_tests(metafunc):
         return
     auto_discovered_rules = [(rule, None, f"{category}/{rule}") for category, rule in get_rules_for_atest()]
     metafunc.parametrize('rule, args, test_data', auto_discovered_rules)
-
-
-def preconfigured_robocop(rules, sources):
-    runner = Robocop(from_cli=True)
-    config = Config()
-    config.parse_opts([
-        '--include',
-        ','.join(rules),
-        '--format',
-        '{source}:{line}:{col} [{severity}] {rule_id} {desc}',
-        '--configure',
-        'return_status:quality_gate:E=0:W=0:I=0',
-        *sources
-     ])
-    runner.config = config
-    return runner

d3184802cee72522f85b4b7632e64f045149e70b
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2021-08-18 13:19:25 +0200

    remove toml not installed test

--- tests/utest/test_default_config.py
+++ tests/utest/test_default_config.py
@@ -125,13 +125,3 @@ class TestDefaultConfig:
         with pytest.raises(InvalidArgumentError) as e, patch.object(sys, 'argv', ['prog']):
             config.parse_opts()
         assert "Invalid configuration for Robocop:\\nFailed to decode " in str(e)
-
-    def test_toml_not_installed_pyproject(self, path_to_test_data):
-        src = path_to_test_data / 'only_pyproject'
-        os.chdir(str(src))
-        with patch.dict('sys.modules', {'toml': None}):
-            importlib.reload(robocop.config)
-            config = robocop.config.Config()
-            with patch.object(sys, 'argv', ['prog']):
-                config.parse_opts()
-            assert config.include == set()

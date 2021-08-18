8b6897c2944451ce27d6fc10b0d5364657c0f59e
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2021-08-18 13:18:09 +0200

    remove deprecated warning

--- robocop/config.py
+++ robocop/config.py
@@ -322,28 +322,9 @@ class Config:
         parse_toml_to_config(config, self)
         self.config_from = pyproject_path
 
-    @staticmethod
-    def replace_in_set(container, old_key, new_key):
-        if old_key not in container:
-            return
-        container.remove(old_key)
-        container.add(new_key)
-
     def validate_rule_names(self, rules):
-        deprecated = {
-            'setting-name-not-capitalized': 'setting-name-not-in-title-case',
-            'not-capitalized-keyword-name': 'wrong-case-in-keyword-name',
-            'missing-doc-testcase': 'missing-doc-test-case'
-        }
         for rule in chain(self.include, self.exclude):
-            # TODO: Remove in 1.9.0
-            if rule in deprecated:
-                print(f"### DEPRECATION WARNING: The name of the rule '{rule}' is "
-                      f"renamed to '{deprecated[rule]}' starting from Robocop 1.8.0. "
-                      f"Update your configuration if you're using old name. ###\n")
-                self.replace_in_set(self.include, rule, deprecated[rule])
-                self.replace_in_set(self.exclude, rule, deprecated[rule])
-            elif rule not in rules:
+            if rule not in rules:
                 similiar = RecommendationFinder().find_similar(rule, rules)
                 raise ConfigGeneralError(f"Provided rule '{rule}' does not exist.{similiar}")
 

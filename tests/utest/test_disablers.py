89f9ca5140edf98f00fd0b94363a24c8c77df4f8
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2021-08-27 11:22:04 +0200

    rename fixture

--- tests/utest/test_disablers.py
+++ tests/utest/test_disablers.py
@@ -20,7 +20,7 @@ def message():
 
 
 @pytest.fixture
-def test_data_dir():
+def disablers_test_data():
     return Path(Path(__file__).parent.parent, 'test_data', 'disabled')
 
 
@@ -40,38 +40,38 @@ def run_check_on_string(in_memory, include=None, configure=None):
 
 
 class TestDisablers:
-    def test_disabled_whole_file(self, test_data_dir):
-        disabler = DisablersFinder(test_data_dir / 'disabled_whole.robot', None)
+    def test_disabled_whole_file(self, disablers_test_data):
+        disabler = DisablersFinder(disablers_test_data / 'disabled_whole.robot', None)
         assert disabler.file_disabled
-        disabler = DisablersFinder(test_data_dir / 'disabled.robot', None)
+        disabler = DisablersFinder(disablers_test_data / 'disabled.robot', None)
         assert not disabler.file_disabled
 
-    def test_is_line_disabled(self, test_data_dir):
-        disabler = DisablersFinder(test_data_dir / 'disabled.robot', None)
+    def test_is_line_disabled(self, disablers_test_data):
+        disabler = DisablersFinder(disablers_test_data / 'disabled.robot', None)
         assert disabler.any_disabler
         assert disabler.is_line_disabled(1, 'all')  # from robocop: disable
         assert disabler.is_line_disabled(11, 'somerule')
         assert disabler.is_line_disabled(13, 'all')  # from noqa
         assert not disabler.is_line_disabled(11, 'otherule')
-        disabler = DisablersFinder(test_data_dir / 'disabled_whole.robot', None)
+        disabler = DisablersFinder(disablers_test_data / 'disabled_whole.robot', None)
         for i in range(1, 11):
             assert disabler.is_line_disabled(i, 'all')
 
-    def test_is_rule_disabled(self, message, test_data_dir):
+    def test_is_rule_disabled(self, message, disablers_test_data):
         # check if rule 1010 is disabled in selected lines
         disabled_lines = {1, 2, 3, 4, 7, 11, 13}
-        disabler = DisablersFinder(test_data_dir / 'disabled.robot', None)
+        disabler = DisablersFinder(disablers_test_data / 'disabled.robot', None)
         for i in range(1, 14):
             message.line = i
             assert disabler.is_rule_disabled(message) == (i in disabled_lines)
 
-    def test_enabled_file(self, test_data_dir):
-        disabler = DisablersFinder(test_data_dir / 'enabled.robot', None)
+    def test_enabled_file(self, disablers_test_data):
+        disabler = DisablersFinder(disablers_test_data / 'enabled.robot', None)
         assert not disabler.any_disabler
 
     @pytest.mark.parametrize('file', [1, 2, 3, 4])
-    def test_extended_disabling(self, file, test_data_dir):
-        source = test_data_dir / f'extended_lines{file}.robot'
+    def test_extended_disabling(self, file, disablers_test_data):
+        source = disablers_test_data / f'extended_lines{file}.robot'
         with open(source) as f:
             data = f.read()
         issues = run_check_on_string(data, include={'too-long-keyword'}, configure=['too-long-keyword:max_len:1'])

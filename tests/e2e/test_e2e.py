e0756388e254c363c37f223c67a91d5a6f0a651b
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2021-09-02 14:35:52 +0200

    use RobotFramework FileReader to avoid encoding issues

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -16,84 +16,47 @@ def robocop_instance_not_cli():
     return Robocop(from_cli=False)
 
 
-class TestE2E:
-    def test_run_all_checkers(self, robocop_instance):
-        config = Config()
-        config.parse_opts([str(Path(Path(__file__).parent.parent, 'test_data'))])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+@pytest.fixture
+def test_data_dir():
+    return Path(Path(__file__).parent.parent, 'test_data')
 
-    def test_run_all_checkers_not_cli(self, robocop_instance_not_cli):
-        robocop_instance_not_cli.config.paths = [str(Path(Path(__file__).parent.parent, 'test_data'))]
+
+def should_run_with_config(robocop_instance, cfg):
+    config = Config()
+    config.parse_opts(cfg.split())
+    robocop_instance.config = config
+    with pytest.raises(SystemExit):
+        robocop_instance.run()
+    return robocop_instance
+
+
+class TestE2E:
+    def test_run_all_checkers(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, str(test_data_dir))
+
+    def test_run_all_checkers_not_cli(self, robocop_instance_not_cli, test_data_dir):
+        robocop_instance_not_cli.config.paths = [str(test_data_dir)]
         issues = robocop_instance_not_cli.run()
         assert issues
         assert isinstance(issues[0], dict)
 
-    def test_run_all_checkers_not_recursive(self, robocop_instance):
-        config = Config()
-        config.parse_opts(['--no-recursive',
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_run_all_checkers_not_recursive(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'--no-recursive {test_data_dir}')
 
-    def test_all_reports(self, robocop_instance):
-        config = Config()
-        config.parse_opts([
-            '-r',
-            'all',
-            str(Path(Path(__file__).parent.parent, 'test_data'))
-        ])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_all_reports(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'-r all {test_data_dir}')
 
-    def test_no_issues_all_reports(self, robocop_instance):
-        config = Config()
-        config.parse_opts([
-            '-r',
-            'all',
-            str(Path(Path(__file__).parent.parent, 'test_data/all_passing.robot'))
-        ])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_no_issues_all_reports(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'-r all {test_data_dir / "all_passing.robot"}')
 
-    def test_disable_all_pattern(self, robocop_instance):
-        config = Config()
-        config.parse_opts([
-            '--exclude',
-            '*',
-            str(Path(Path(__file__).parent.parent, 'test_data'))
-        ])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_disable_all_pattern(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'--exclude * {test_data_dir}')
 
-    def test_ignore_file_with_pattern(self, robocop_instance):
-        config = Config()
-        config.parse_opts([
-            '--ignore',
-            '*.robot',
-            '--include',
-            '0502',
-            str(Path(Path(__file__).parent.parent, 'test_data'))
-        ])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_ignore_file_with_pattern(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'--ignore *.robot --include 0502 {test_data_dir}')
 
-    def test_include_one_rule(self, robocop_instance):
-        config = Config()
-        config.parse_opts([
-            '--include',
-            '0503',
-            str(Path(Path(__file__).parent.parent, 'test_data'))
-        ])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_include_one_rule(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'--include 0503 {test_data_dir}')
 
     def test_run_non_existing_file(self, robocop_instance):
         config = Config()
@@ -103,39 +66,22 @@ class TestE2E:
             robocop_instance.run()
         assert 'File "some_path" does not exist' in str(err)
 
-    def test_run_with_return_status_0(self, robocop_instance):
-        config = Config()
-        config.parse_opts(['-c', 'return_status:quality_gate:E=-1:W=-1',
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
-        assert robocop_instance.reports['return_status'].return_status == 0
+    def test_run_with_return_status_0(self, robocop_instance, test_data_dir):
+        runner = should_run_with_config(robocop_instance, f'-c return_status:quality_gate:E=-1:W=-1 {test_data_dir}')
+        assert runner.reports['return_status'].return_status == 0
 
-    def test_run_with_return_status_bigger_than_zero(self, robocop_instance):
-        config = Config()
-        config.parse_opts(['--configure', 'return_status:quality_gate:E=0:W=0',
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
-        assert robocop_instance.reports['return_status'].return_status > 0
+    def test_run_with_return_status_bigger_than_zero(self, robocop_instance, test_data_dir):
+        runner = should_run_with_config(
+            robocop_instance,
+            f'--configure return_status:quality_gate:E=0:W=0 {test_data_dir}'
+        )
+        assert runner.reports['return_status'].return_status > 0
 
-    def test_configure_rule_severity(self, robocop_instance):
-        config = Config()
-        config.parse_opts(['-c', '0201:severity:E', '-c', 'E0202:severity:I',
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_configure_rule_severity(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'-c 0201:severity:E -c E0202:severity:I {test_data_dir}')
 
-    def test_configure_rule_option(self, robocop_instance):
-        config = Config()
-        config.parse_opts(['-c', 'line-too-long:line_length:1000',
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_configure_rule_option(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'-c line-too-long:line_length:1000 {test_data_dir}')
 
     @pytest.mark.parametrize('rule, expected', [
         ('idontexist', "Provided rule or report 'idontexist' does not exist."),
@@ -144,10 +90,9 @@ class TestE2E:
          r"Did you mean:\n    not-enough-whitespace-after-newline-marker"
          )
     ])
-    def test_configure_invalid_rule(self, robocop_instance, rule, expected):
+    def test_configure_invalid_rule(self, robocop_instance, rule, expected, test_data_dir):
         config = Config()
-        config.parse_opts(['--configure', f'{rule}:severity:E',
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
+        config.parse_opts(['--configure', f'{rule}:severity:E', str(test_data_dir)])
         robocop_instance.config = config
         robocop_instance.load_checkers()
         with pytest.raises(ConfigGeneralError) as err:
@@ -168,10 +113,9 @@ class TestE2E:
                 robocop_instance.reload_config()
             assert expected in str(err)
 
-    def test_configure_invalid_param(self, robocop_instance):
+    def test_configure_invalid_param(self, robocop_instance, test_data_dir):
         config = Config()
-        config.parse_opts(['--configure', '0202:idontexist:E',
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
+        config.parse_opts(['--configure', '0202:idontexist:E', str(test_data_dir)])
         robocop_instance.config = config
         robocop_instance.load_checkers()
         with pytest.raises(ConfigGeneralError) as err:
@@ -179,47 +123,34 @@ class TestE2E:
         assert r"Provided param 'idontexist' for rule '0202' does not exist. " \
                r"Available configurable(s) for this rule:\n    severity" in str(err)
 
-    def test_configure_invalid_config(self, robocop_instance):
+    def test_configure_invalid_config(self, robocop_instance, test_data_dir):
         config = Config()
-        config.parse_opts(['--configure', '0202:',
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
+        config.parse_opts(['--configure', '0202:', str(test_data_dir)])
         robocop_instance.config = config
         robocop_instance.load_checkers()
         with pytest.raises(ConfigGeneralError) as err:
             robocop_instance.configure_checkers_or_reports()
         assert "Provided invalid config: '0202:' (general pattern: <rule>:<param>:<value>)" in str(err)
 
-    def test_configure_return_status_invalid_value(self, robocop_instance):
-        config = Config()
-        config.parse_opts(['--configure', 'return_status:quality_gate:E0',
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_configure_return_status_invalid_value(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'--configure return_status:quality_gate:E0 {test_data_dir}')
 
     def test_configure_return_status_with_non_exist(self, robocop_instance):
         config = Config()
-        config.parse_opts(['--configure', 'return_status:smth:E=0:W=0',
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
+        config.parse_opts(['--configure', 'return_status:smth:E=0:W=0', str(test_data_dir)])
         robocop_instance.config = config
         robocop_instance.load_reports()
         with pytest.raises(ConfigGeneralError) as err:
             robocop_instance.configure_checkers_or_reports()
         assert "Provided param 'smth' for report 'return_status' does not exist" in str(err)
 
-    def test_use_argument_file(self, robocop_instance):
-        config = Config()
-        config.parse_opts(['-A', str(Path(Path(__file__).parent.parent, 'test_data/argument_file/args.txt')),
-                           str(Path(Path(__file__).parent.parent, 'test_data'))])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_use_argument_file(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'-A {test_data_dir / "argument_file" / "args.txt"}')
 
-    def test_use_not_existing_argument_file(self):
+    def test_use_not_existing_argument_file(self, test_data_dir):
         config = Config()
         with pytest.raises(ArgumentFileNotFoundError) as err:
-            config.parse_opts(['--argumentfile', 'some_file',
-                               str(Path(Path(__file__).parent.parent, 'test_data'))])
+            config.parse_opts(['--argumentfile', 'some_file', str(test_data_dir)])
         assert 'Argument file "some_file" does not exist' in str(err)
 
     def test_argument_file_without_path(self):
@@ -228,46 +159,29 @@ class TestE2E:
             config.parse_opts(['--argumentfile'])
         assert 'Argument file "" does not exist' in str(err)
 
-    def test_use_nested_argument_file(self):
+    def test_use_nested_argument_file(self, test_data_dir):
         config = Config()
-        nested_args_path = str(Path(Path(__file__).parent.parent, 'test_data/argument_file/args_nested.txt'))
+        nested_args_path = str( test_data_dir / 'argument_file' / 'args_nested.txt')
         with pytest.raises(NestedArgumentFileError) as err:
-            config.parse_opts(['-A', nested_args_path,
-                               str(Path(Path(__file__).parent.parent, 'test_data'))])
+            config.parse_opts(['-A', nested_args_path, str(test_data_dir)])
         assert 'Nested argument file in ' in str(err)
 
-    def test_set_rule_threshold(self, robocop_instance):
-        config = Config()
-        config.parse_opts([
-            '--threshold',
-            'E',
-            str(Path(Path(__file__).parent.parent, 'test_data'))
-        ])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_set_rule_threshold(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'--threshold E {test_data_dir}')
 
-    def test_set_rule_invalid_threshold(self, robocop_instance):
-        config = Config()
-        config.parse_opts([
-            '--threshold',
-            '3',
-            str(Path(Path(__file__).parent.parent, 'test_data'))
-        ])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+    def test_set_rule_invalid_threshold(self, robocop_instance, test_data_dir):
+        should_run_with_config(robocop_instance, f'--threshold 3 {test_data_dir}')
 
-    def test_configure_severity(self, robocop_instance):
+    def test_configure_severity(self, robocop_instance, test_data_dir):
         # issue 402
-        config = Config()
-        config.parse_opts([
-            '-c',
-            'wrong-case-in-keyword-name:severity:E',
-            '-c',
-            'wrong-case-in-keyword-name:convention:first_word_capitalized',
-            str(Path(Path(__file__).parent.parent, 'test_data'))
-        ])
-        robocop_instance.config = config
-        with pytest.raises(SystemExit):
-            robocop_instance.run()
+        should_run_with_config(
+            robocop_instance,
+            f'-c wrong-case-in-keyword-name:severity:E -c wrong-case-in-keyword-name:convention:first_word_capitalized'
+            f'{test_data_dir}'
+        )
+
+    def test_diff_encoded_chars(self, robocop_instance, test_data_dir, capsys):
+        # issue 455
+        should_run_with_config(robocop_instance, str(test_data_dir / 'encodings.robot'))
+        out, _ = capsys.readouterr()
+        assert 'Failed to decode' not in out
---
+++ tests/test_data/encodings.robot
@@ -0,0 +1,17 @@
+*** Test Cases ***
+Test Czech
+    [Documentation]  ÁáÉéĚěÍíÓóÚúÝýŮůŘřŤťŠšĎďŽžČčŇň
+    No Operation
+
+Test Slovak
+    [Documentation]  ĽľĹĺŔŕÄäô
+    No Operation
+
+Test Polish
+    [Documentation]  ąęłżźćóńĄĘŻŹÓŁĆ
+    No Operation
+
+Test Other
+    [Documentation]    Eäi Saa Peittää
+    ...                日本語
+    No Operation

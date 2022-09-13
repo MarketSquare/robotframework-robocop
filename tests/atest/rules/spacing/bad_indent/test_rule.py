c3f538d32a7a0a7e9e05161d7ffa9b116d1a2598
Bartlomiej Hirsz <Bartlomiej_Hirsz@epam.com>
2022-09-13 19:55:02 +0200

    Migrate spacing rules atests to new framework

---
+++ tests/atest/rules/spacing/bad_indent/test_rule.py
@@ -0,0 +1,12 @@
+from tests.atest.utils import RuleAcceptance
+
+
+class TestRuleAcceptance(RuleAcceptance):
+    def test_rule(self):
+        self.check_rule(src_files=["test.robot"], expected_file="expected_output.txt", target_version=">=5")
+
+    def test_rf4(self):
+        self.check_rule(src_files=["test.robot"], expected_file="expected_output_rf4.txt", target_version="==4.*")
+
+    def test_rf3(self):
+        self.check_rule(src_files=["test.robot"], expected_file="expected_output_rf3.txt", target_version="==3.*")

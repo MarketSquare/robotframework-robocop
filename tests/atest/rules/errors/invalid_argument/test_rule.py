2720f2ed06bc6bb20a9b7e5948d74f6b34ef89f9
Bartłomiej Hirsz <Bartlomiej_Hirsz@epam.com>
2022-09-15 19:41:29 +0200

    Provide end line/col information to invalid-argument rule (#729)
    * Provide end line/col information to invalid-argument rule
    
    * Apply suggestions from code review
    
    Co-authored-by: Mateusz Nojek <matnojek@gmail.com>

--- tests/atest/rules/errors/invalid_argument/test_rule.py
+++ tests/atest/rules/errors/invalid_argument/test_rule.py
@@ -3,4 +3,6 @@ from tests.atest.utils import RuleAcceptance
 
 class TestRuleAcceptance(RuleAcceptance):
     def test_rule(self):
-        self.check_rule(src_files=["test.robot"], expected_file="expected_output.txt", target_version=">=4")
+        self.check_rule(
+            src_files=["test.robot"], expected_file="expected_output.txt", target_version=">=4", issue_format="end_col"
+        )

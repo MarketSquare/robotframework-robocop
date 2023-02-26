2752d814c58c07b39c652f576d01dc9e2e77e88e
Mateusz Nojek <matnojek@gmail.com>
2023-02-26 13:10:25 +0100

    Add rule W0204 for missing doc in resource file (#783)
    * Add 0204 rule for missing doc in Resource files
    
    * Add another test file for new rule

---
+++ tests/atest/rules/documentation/missing_doc_resource_file/test_rule.py
@@ -0,0 +1,6 @@
+from tests.atest.utils import RuleAcceptance
+
+
+class TestRuleAcceptance(RuleAcceptance):
+    def test_rule(self):
+        self.check_rule(src_files=["test.robot", "test.resource", "documentation.resource"], expected_file="expected_output.txt")

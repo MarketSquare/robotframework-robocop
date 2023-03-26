6426703bab4abec3b2b892e314401afd033db04c
Mateusz Nojek <matnojek@gmail.com>
2023-03-26 19:35:33 +0200

    Fix unit tests by overwriting default configuration

--- tests/atest/rules/spacing/bad_indent/test_rule.py
+++ tests/atest/rules/spacing/bad_indent/test_rule.py
@@ -18,4 +18,8 @@ class TestRuleAcceptance(RuleAcceptance):
 
     @pytest.mark.parametrize("file_suffix, target_version", [("", ">=5"), ("_rf4", "==4.*"), ("_rf3", "==3.*")])
     def test_rule(self, file_suffix, target_version):
-        self.check_rule(expected_file=f"expected_output{file_suffix}.txt", target_version=target_version)
+        self.check_rule(
+            config="-c bad-indent:indent:-1",
+            expected_file=f"expected_output{file_suffix}.txt",
+            target_version=target_version,
+        )

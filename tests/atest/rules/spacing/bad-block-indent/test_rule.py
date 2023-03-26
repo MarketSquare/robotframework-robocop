a98fb40126a2991969686829743ecf173158218b
Mateusz Nojek <matnojek@gmail.com>
2023-03-26 20:22:28 +0200

    Update tests output for older RF versions

--- tests/atest/rules/spacing/bad-block-indent/test_rule.py
+++ tests/atest/rules/spacing/bad-block-indent/test_rule.py
@@ -4,5 +4,9 @@ from tests.atest.utils import RuleAcceptance
 
 
 class TestRuleAcceptance(RuleAcceptance):
-    def test_rule(self):
-        self.check_rule(expected_file="expected_output.txt")
+    @pytest.mark.parametrize("file_suffix, target_version", [("", ">=5"), ("_rf4", "==4.*"), ("_rf3", "==3.*")])
+    def test_rule(self, file_suffix, target_version):
+        self.check_rule(
+            expected_file=f"expected_output{file_suffix}.txt",
+            target_version=target_version,
+        )

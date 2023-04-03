d2998daf83c521740132fb785e1fd6760b086e52
Mateusz Nojek <matnojek@gmail.com>
2023-04-03 11:10:05 +0200

    Fix tests

--- tests/atest/rules/misc/unreachable_code/test_rule.py
+++ tests/atest/rules/misc/unreachable_code/test_rule.py
@@ -5,4 +5,5 @@ class TestRuleAcceptance(RuleAcceptance):
     def test_rule_before(self):
         self.check_rule(
             expected_file="expected_output.txt",
+            target_version=">=5.0",
         )

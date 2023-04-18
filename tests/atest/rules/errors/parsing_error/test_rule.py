7139cc1000430020d5ea1e9b6e01fd3800b84f60
Bartlomiej Hirsz <bartlomiej.hirsz@iqvia.com>
2023-04-18 09:32:36 +0200

    Update tests with language for old Robot Framework versions

--- tests/atest/rules/errors/parsing_error/test_rule.py
+++ tests/atest/rules/errors/parsing_error/test_rule.py
@@ -3,7 +3,10 @@ from tests.atest.utils import RuleAcceptance
 
 class TestRule(RuleAcceptance):
     def test_rule(self):
-        self.check_rule(expected_file="expected_output.txt", target_version=">=5.0")
+        self.check_rule(expected_file="expected_output.txt", target_version=">=6.0")
+
+    def test_rule_rf5(self):
+        self.check_rule(expected_file="expected_output_rf5.txt", target_version="==5.*")
 
     def test_rule_rf4(self):
         self.check_rule(expected_file="expected_output_rf4.txt", target_version="==4.1.3")

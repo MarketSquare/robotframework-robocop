65e209f58bbbf08f5904ac31a35e138150e95907
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2023-04-22 15:29:53 +0200

    Fix tests for RF3-RF4

--- tests/atest/rules/misc/argument_overwritten_before_usage/test_rule.py
+++ tests/atest/rules/misc/argument_overwritten_before_usage/test_rule.py
@@ -3,4 +3,14 @@ from tests.atest.utils import RuleAcceptance
 
 class TestRuleAcceptance(RuleAcceptance):
     def test_rule(self):
-        self.check_rule(src_files=["test.robot"], expected_file="expected_output.txt", issue_format="end_col")
+        self.check_rule(
+            src_files=["test.robot"], expected_file="expected_output.txt", issue_format="end_col", target_version=">=5"
+        )
+
+    def test_rule_rf3_rf4(self):
+        self.check_rule(
+            src_files=["test.robot"],
+            expected_file="expected_output_rf3.txt",
+            issue_format="end_col",
+            target_version="<5",
+        )

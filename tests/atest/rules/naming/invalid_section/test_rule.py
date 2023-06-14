ce9735f35c3c1d5403161a343974e8e14770c3f0
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2023-06-14 13:03:40 +0200

    Disable invalid-section tests for Robocop older versions

--- tests/atest/rules/naming/invalid_section/test_rule.py
+++ tests/atest/rules/naming/invalid_section/test_rule.py
@@ -3,4 +3,6 @@ from tests.atest.utils import RuleAcceptance
 
 class TestRuleAcceptance(RuleAcceptance):
     def test_rule(self):
-        self.check_rule(src_files=["test.robot", "test_lang.resource"], expected_file="expected_output.txt")
+        self.check_rule(
+            src_files=["test.robot", "test_lang.resource"], expected_file="expected_output.txt", target_version=">=6.1"
+        )

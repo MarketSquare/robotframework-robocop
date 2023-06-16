59a62fc1d0e368261cc60e9a89ad50927eade5e4
Mateusz Nojek <matnojek@gmail.com>
2023-06-16 13:38:33 +0200

    Linting fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 # This file's here to allow running specific tests using Pytest command
 
-from pathlib import Path
 import sys
-
+from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))

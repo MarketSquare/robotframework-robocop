31165e7112698d5e66d9aa86f5017bb18aec5ea9
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2023-08-09 09:51:29 +0200

    Fix threshold filtering out rules with severity threshold using default severity

--- tests/utest/test_thresholds.py
+++ tests/utest/test_thresholds.py
@@ -16,6 +16,20 @@ def get_message_with_id_sev(rule_id, sev):
     )
 
 
+def get_message_with_sev_value(rule, sev_value):
+    return rule.prepare_message(
+        source=None,
+        node=None,
+        lineno=1,
+        end_lineno=1,
+        col=1,
+        end_col=1,
+        sev_threshold_value=sev_value,
+        extended_disablers=None,
+        severity=None,
+    )
+
+
 class TestThresholds:
     @pytest.mark.parametrize(
         "threshold, included, excluded",
@@ -35,6 +49,9 @@ class TestThresholds:
             assert not robocop_pre_load.config.is_rule_enabled(msg)
 
 
+# filter messages properly - if -t E, severity threshold returns W -> should filter
+
+
 class TestRuleSeverityThreshold:
     def test_rule_threshold(self):
         thresholds = SeverityThreshold("line_length")
@@ -45,6 +62,25 @@ class TestRuleSeverityThreshold:
         assert thresholds.get_severity(101) == RuleSeverity.WARNING
         assert thresholds.get_severity(149) == RuleSeverity.WARNING
         assert thresholds.get_severity(150) == RuleSeverity.ERROR
+
+    def test_rule_severity_threshold_and_filter_threshold(self):
+        rule = Rule(
+            RuleParam(name="param_name", converter=int, default=1, desc=""),
+            SeverityThreshold("param_name", compare_method="greater"),
+            rule_id="0101",
+            name=f"some-message",
+            msg="Some description",
+            severity=RuleSeverity.WARNING,
+        )
+        rule.config["severity_threshold"].value = "W=0:E=1"
+        msg_1 = get_message_with_sev_value(rule, 0)
+        msg_2 = get_message_with_sev_value(rule, 1)
+        msg_3 = get_message_with_sev_value(rule, 2)
+        assert [msg_1.severity, msg_2.severity, msg_3.severity] == [
+            RuleSeverity.WARNING,
+            RuleSeverity.ERROR,
+            RuleSeverity.ERROR,
+        ]
 
     def test_invalid_threshold_config(self):
         thresholds = SeverityThreshold("line_length")

63dd8733aa45c308517df634ed7f7168fc95907a
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2023-11-12 15:56:37 +0100

    Fix setting-not-supported test data to support RF 7 [Setup] allowed in keywords

--- tests/atest/rules/errors/setting_not_supported/test_rule.py
+++ tests/atest/rules/errors/setting_not_supported/test_rule.py
@@ -4,3 +4,6 @@ from tests.atest.utils import RuleAcceptance
 class TestRuleAcceptance(RuleAcceptance):
     def test_rule(self):
         self.check_rule(src_files=["test.robot"], expected_file="expected_output.txt")
+
+    def test_pre_rf7(self):
+        self.check_rule(src_files=["test.robot"], expected_file="expected_output_pre7.txt", target_version="<7.0")

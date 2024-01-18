2e0a7d4366961568672dce2cdd1379e3a6451cfc
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2024-01-18 13:15:51 +0100

    Fix missing-doc-resource-file not raised with files with settings sections

--- tests/atest/rules/documentation/missing_doc_resource_file/test_rule.py
+++ tests/atest/rules/documentation/missing_doc_resource_file/test_rule.py
@@ -4,5 +4,6 @@ from tests.atest.utils import RuleAcceptance
 class TestRuleAcceptance(RuleAcceptance):
     def test_rule(self):
         self.check_rule(
-            src_files=["test.robot", "test.resource", "documentation.resource"], expected_file="expected_output.txt"
+            src_files=["test.robot", "test.resource", "documentation.resource", "with_settings.resource"],
+            expected_file="expected_output.txt",
         )

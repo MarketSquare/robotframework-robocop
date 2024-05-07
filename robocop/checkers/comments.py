0a77ff6ded0245fa8f7e1022ea9025aa8c2b3dae
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2024-05-07 09:19:34 +0200

    Fix ignored-data raised for files with only robocop disablers

--- robocop/checkers/comments.py
+++ robocop/checkers/comments.py
@@ -255,12 +255,12 @@ class IgnoredDataChecker(RawFileChecker):
 
     def __init__(self):
         self.is_bom = False
-        self.has_language_header = False
+        self.ignore_empty_lines = False  # ignore empty lines if language header or robocop disabler is present
         super().__init__()
 
     def parse_file(self):
         self.is_bom = False
-        self.has_language_header = False
+        self.ignore_empty_lines = False
         if self.lines is not None:
             for lineno, line in enumerate(self.lines, start=1):
                 if self.check_line(line, lineno):
@@ -276,16 +276,16 @@ class IgnoredDataChecker(RawFileChecker):
         if line.startswith(self.SECTION_HEADER):
             return True
         if line.startswith(self.ROBOCOP_HEADER):
+            self.ignore_empty_lines = True
             return False
         if lineno == 1:
             if line.lower().startswith(self.LANGUAGE_HEADER):
-                self.has_language_header = True
+                self.ignore_empty_lines = True
                 return False
             if self.is_bom:
                 # if it's BOM encoded file, first line can be ignored
                 return "***" in line
-        if self.has_language_header and not line.strip():
-            # empty lines after language: header can be ignored
+        if self.ignore_empty_lines and not line.strip():
             return False
         self.report("ignored-data", lineno=lineno, col=1, end_col=len(line))
         return True

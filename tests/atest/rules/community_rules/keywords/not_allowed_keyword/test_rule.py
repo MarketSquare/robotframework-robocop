4b18badb9f0113e73c53023175231d308fa20735
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2024-05-09 12:14:27 +0200

    Update tests for RF3 and RF4

--- tests/atest/rules/community_rules/keywords/not_allowed_keyword/test_rule.py
+++ tests/atest/rules/community_rules/keywords/not_allowed_keyword/test_rule.py
@@ -8,4 +8,23 @@ class TestRuleAcceptance(RuleAcceptance):
             expected_file="expected_output.txt",
             config="-c not-allowed-keyword:keywords:NotAllowed,Not_AllowedWithArgs,library.not_allowed_with_lib",
             issue_format="end_col",
+            target_version=">=5",
+        )
+
+    def test_rule_rf3(self):
+        self.check_rule(
+            src_files=["test.robot"],
+            expected_file="expected_output_rf3.txt",
+            config="-c not-allowed-keyword:keywords:NotAllowed,Not_AllowedWithArgs,library.not_allowed_with_lib",
+            issue_format="end_col",
+            target_version="==3.*",
+        )
+
+    def test_rule_rf4(self):
+        self.check_rule(
+            src_files=["test.robot"],
+            expected_file="expected_output_rf4.txt",
+            config="-c not-allowed-keyword:keywords:NotAllowed,Not_AllowedWithArgs,library.not_allowed_with_lib",
+            issue_format="end_col",
+            target_version="==4.*",
         )

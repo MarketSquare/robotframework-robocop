8005c4589e754a8840263df6141f283a0fdd9a5b
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2025-02-26 18:27:23 +0100

    Fix ruff issues

---
+++ tests/linter/rules/keywords/unused_keyword/test_rule.py
@@ -0,0 +1,9 @@
+import pytest
+
+from tests.linter.utils import RuleAcceptance
+
+
+class TestRuleAcceptance(RuleAcceptance):
+    @pytest.mark.xfail(reason="Project checker needs to be reimplemented")  # FIXME:
+    def test_rule(self):
+        self.check_rule(src_files=["."], expected_file="expected_output.txt", issue_format="end_col")

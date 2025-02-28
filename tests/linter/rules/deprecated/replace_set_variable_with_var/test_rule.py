1346893f570d1db6710002e12f4dcd51364b5803
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2025-02-28 10:29:33 +0100

    Add tests for extended view for deprecated group rules

--- tests/linter/rules/deprecated/replace_set_variable_with_var/test_rule.py
+++ tests/linter/rules/deprecated/replace_set_variable_with_var/test_rule.py
@@ -6,3 +6,6 @@ class TestRuleAcceptance(RuleAcceptance):
         self.check_rule(
             src_files=["test.robot"], expected_file="expected_output.txt", issue_format="end_col", test_on_version=">=7"
         )
+
+    def test_extended(self):
+        self.check_rule(expected_file="expected_extended.txt", output_format="extended", test_on_version=">=7")

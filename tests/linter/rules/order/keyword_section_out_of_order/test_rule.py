f30f6c5d28b1fb223a6b4e7332a919091cc4ec94
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2025-02-28 18:29:22 +0100

    Add tests for extended view for order rules & refactor issues locations

--- tests/linter/rules/order/keyword_section_out_of_order/test_rule.py
+++ tests/linter/rules/order/keyword_section_out_of_order/test_rule.py
@@ -5,6 +5,9 @@ class TestRuleAcceptance(RuleAcceptance):
     def test_rule(self):
         self.check_rule(src_files=["test.robot"], expected_file="expected_output.txt")
 
+    def test_extended(self):
+        self.check_rule(src_files=["test.robot"], expected_file="expected_extended.txt", output_format="extended")
+
     def test_rule_setup(self):
         self.check_rule(
             src_files=["test.robot", "test_kw_setup_added_in_rf7.robot"],
--- tests/linter/rules/order/test_case_section_out_of_order/test_rule.py
+++ tests/linter/rules/order/test_case_section_out_of_order/test_rule.py
@@ -5,6 +5,9 @@ class TestRuleAcceptance(RuleAcceptance):
     def test_rule(self):
         self.check_rule(expected_file="expected_output.txt")
 
+    def test_extended(self):
+        self.check_rule(src_files=["test.robot"], expected_file="expected_extended.txt", output_format="extended")
+
     def test_rule_teardown_keyword(self):
         self.check_rule(
             configure=["test-case-section-out-of-order.sections_order=teardown,keyword"],

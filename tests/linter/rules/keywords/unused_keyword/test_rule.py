55403e8a9e88465d501f071319f74033e5a238f2
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2025-03-01 14:44:27 +0100

    Add tests for extended view for keywords and whitespace rules

--- tests/linter/rules/keywords/unused_keyword/test_rule.py
+++ tests/linter/rules/keywords/unused_keyword/test_rule.py
@@ -7,3 +7,6 @@ class TestRuleAcceptance(RuleAcceptance):
     @pytest.mark.xfail(reason="Project checker needs to be reimplemented")  # FIXME:
     def test_rule(self):
         self.check_rule(src_files=["."], expected_file="expected_output.txt", issue_format="end_col")
+
+    def test_extended(self):
+        self.check_rule(src_files=["."], expected_file="expected_extended.txt", output_format="extended")
--- tests/linter/rules/whitespace/not_enough_whitespace_after_variable/test_rule.py
+++ tests/linter/rules/whitespace/not_enough_whitespace_after_variable/test_rule.py
@@ -4,3 +4,11 @@ from tests.linter.utils import RuleAcceptance
 class TestRuleAcceptance(RuleAcceptance):
     def test_rule(self):
         self.check_rule(src_files=["test.robot"], expected_file="expected_output.txt", test_on_version=">=4")
+
+    def test_extended(self):
+        self.check_rule(
+            src_files=["test.robot"],
+            expected_file="expected_extended.txt",
+            output_format="extended",
+            test_on_version=">=4",
+        )

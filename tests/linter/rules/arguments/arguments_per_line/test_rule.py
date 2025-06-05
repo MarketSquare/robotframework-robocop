18cf802e65b1728e7f2940b4f1fb020842052b03
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2025-06-05 14:53:19 +0200

    Allow to configure issue format in extended output format (#1411)

--- tests/linter/rules/arguments/arguments_per_line/test_rule.py
+++ tests/linter/rules/arguments/arguments_per_line/test_rule.py
@@ -8,6 +8,14 @@ class TestRuleAcceptance(RuleAcceptance):
     def test_extended(self):
         self.check_rule(src_files=["test.robot"], expected_file="expected_extended.txt", output_format="extended")
 
+    def test_extended_overwrite_issue_format(self):
+        self.check_rule(
+            src_files=["test.robot"],
+            expected_file="expected_extended_configured.txt",
+            output_format="extended",
+            configure=["print_issues.issue_format={source}"],
+        )
+
     def test_rule_max_2(self):
         self.check_rule(
             src_files=["test.robot"],

77ad7b6da213740ae8ffa48f49b534a28d05d7b7
Philipp <11837635+p-zander@users.noreply.github.com>
2025-08-25 11:44:11 +0200

    Change test data to reflect a change in robot error message in RF7.3 (#1445)
    https://github.com/robotframework/robotframework/commit/448fb07

--- tests/linter/rules/errors/invalid_for_loop/test_rule.py
+++ tests/linter/rules/errors/invalid_for_loop/test_rule.py
@@ -3,15 +3,18 @@ from tests.linter.utils import RuleAcceptance
 
 class TestRuleAcceptance(RuleAcceptance):
     def test_rule(self):
-        self.check_rule(src_files=["test.robot"], expected_file="expected_output_rf5.txt", test_on_version=">=5")
+        self.check_rule(src_files=["test.robot"], expected_file="expected_output_rf5.txt", test_on_version=">=5;<7.3")
 
     def test_rf4(self):
         self.check_rule(src_files=["test.robot"], expected_file="expected_output.txt", test_on_version="==4.*")
+
+    def test_rf73(self):
+        self.check_rule(src_files=["test.robot"], expected_file="expected_output_rf73.txt", test_on_version=">=7.3")
 
     def test_extended(self):
         self.check_rule(
             src_files=["test.robot"],
             expected_file="expected_extended.txt",
             output_format="extended",
-            test_on_version=">=5",
+            test_on_version=">=7.3",
         )
